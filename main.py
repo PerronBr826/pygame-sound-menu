bd56a14a6ad43bb0890efa417c97276d895d30e2
PerronBr826 <PerronBr826@northwested.org>
2025-04-21 11:15:01 -0400

    added stuff

--- main.py
+++ main.py
@@ -25,12 +25,16 @@ TITLE = "Pygame Template"
 
 # Frame rate (frames per second)
 FPS = 60
+screenicon = pygame.image.load("soundicon.png")
+screenicon = pygame.transform.scale(screenicon, [200,200])
 
 def init_game ():
     pygame.init()
     pygame.font.init()
     pygame.mixer.init()
     screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT)) # Use constants from config
+
+    pygame.display.set_icon(screenicon)
 
     pygame.display.set_caption(TITLE)
     return screen
@@ -42,15 +46,23 @@ def main():
     char_pose = [WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2]
     char_radius = 50
 
-    sound1 = pygame.mixer.Sound("Sounds/Coins.ogg")
-    sound2 = pygame.mixer.Sound("Sounds/WindowBreak.ogg")
-    gameover = pygame.mixer.Sound("Sounds/GameOver.ogg")
-    scream = pygame.mixer.Sound("Sounds/ScreamMale.ogg")
+    music_dir = "punishment.mp3"
+
+    sound1 = pygame.mixer.Sound("Sounds/coins.ogg")
+    sound2 = pygame.mixer.Sound("Sounds/windowbreak.ogg")
+    gameover = pygame.mixer.Sound("Sounds/gameover.ogg")
+    scream = pygame.mixer.Sound("Sounds/screammale.ogg")
+    music = pygame.mixer.music.load(music_dir)
+
+    music_len = pygame.mixer.Sound(music_dir).get_length()
 
     def darken(color, dark):
         return [pygame.math.clamp(color[0] * dark, 0, 255),pygame.math.clamp(color[1] * dark, 0, 255),pygame.math.clamp(color[2] * dark, 0, 255),]
 
-    buttons = [{"text" : "Play Sound", "hover" : 0, "color" : (155,155,0), "purpose" : "sound1"}, {"text" : "Play Sound 2", "hover" : 0, "color" : (155,155,0), "purpose" : "sound2"}, {"text" : "Close", "hover" : 0, "color" : (155,55,50), "purpose" : "close"},
+    buttons = [{"text" : "Sound 1", "hover" : 0, "color" : (155,155,0), "purpose" : "sound1"}, {"text" : "Sound 2", "hover" : 0, "color" : (155,155,0), "purpose" : "sound2"},
+               {"text" : "Music Play", "hover" : 0, "color" : (55,255,50), "purpose" : "playmusic"},
+               {"text" : "Pause", "hover" : 0, "color" : (255,255,50), "purpose" : "pausemusic"},
+               {"text" : "Music Stop", "hover" : 0, "color" : (55,55,250), "purpose" : "stopmusic"},
                {"text" : "Close", "hover" : 0, "color" : (155,55,50), "purpose" : "close"},
                ]
 
@@ -81,6 +93,21 @@ def main():
                             sound1.play()
                         elif button["purpose"] == "sound2":
                             sound2.play()
+                        elif button["purpose"] == "playmusic":
+                            if pygame.mixer.music.get_pos() > 0:
+                                pygame.mixer.music.unpause()
+                            else: 
+                                pygame.mixer.music.play()
+
+                        elif button["purpose"] == "pausemusic":
+                            if pygame.mixer.music.get_busy() == True:
+                                pygame.mixer.music.pause()
+                            else:
+                                pygame.mixer.music.unpause()
+
+                        elif button["purpose"] == "stopmusic":
+                            pygame.mixer.music.rewind()
+                            pygame.mixer.music.stop()
 
                         print(button["text"])
 
@@ -106,19 +133,19 @@ def main():
 
 
             if button_box.collidepoint(mouse_x, mouse_y):
-                button_color = darken(button["color"], 0.5)
+                button_color = darken(button["color"], 0.6)
                 button["hover"] = pygame.math.clamp((button["hover"] + 1) * 1.2, 0, 30)
 
             else:
-                button_color = darken(button["color"], 0.6)
+                button_color = original_color
                 button["hover"] = pygame.math.clamp((button["hover"] + 1) * 0.9, 0, 30)
 
 
-            button_font = pygame.font.SysFont("Comic Sans MS", round(pygame.math.clamp((20 + button["hover"] * 0.9), 40, 76)))
-            button_font2 = pygame.font.SysFont("Comic Sans MS", round(pygame.math.clamp((20 + button["hover"] * 0.7), 40, 76)))
+            button_font = pygame.font.SysFont("Comic Sans MS", round(pygame.math.clamp((40 + button["hover"] * 0.3), 40, 76)))
+            button_font2 = pygame.font.SysFont("Comic Sans MS", round(pygame.math.clamp((40 + button["hover"] * 0.6), 40, 76)))
                 
-            button_label = button_font.render(button["text"], True, darken(button_color, 0.8))
-            button_label2 = button_font2.render(button["text"], True, darken(button_color, 0.5))
+            button_label = button_font.render(button["text"], True, darken(button_color, 0.6))
+            button_label2 = button_font2.render(button["text"], True, darken(original_color, 0.7))
 
             faux_buttonx = BUTTON_WIDTH + button["hover"]
             faux_buttony = BUTTON_HEIGHT + button["hover"] / 2
@@ -133,12 +160,23 @@ def main():
             text_rect2.center = button_box.center
 
 
-            pygame.draw.rect(screen, original_color, button_box)
-            screen.blit(button_label2, text_rect2)
+            
+
+            pygame.draw.rect(screen, button_color, button_box)
             screen.blit(button_label, text_rect)
+            screen.blit(button_label2, text_rect2)
+
+            screen.blit(screenicon, (WINDOW_WIDTH - 220, 20, 100, 100))
 
             button_i += 1
-     
+
+        bar_origin = [WINDOW_WIDTH - 500, WINDOW_HEIGHT - 100]
+
+        completion = ((pygame.mixer.music.get_pos()/1000) / music_len)
+        
+        pygame.draw.rect(screen, (55,55,55), [bar_origin[0], bar_origin[1], 400, 4])
+        pygame.draw.rect(screen, (255,55,55), [bar_origin[0], bar_origin[1], 400 * completion, 4])
+        pygame.draw.rect(screen, (155,155,155), [bar_origin[0] + (400 * completion) - 3, bar_origin[1] - 5, 6, 14])
 
         pygame.display.flip()
         # Limit the frame rate to the specified frames per second (FPS)
